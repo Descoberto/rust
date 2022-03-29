937c97cc84e6d24d60e8775b1717f42f9eacfc71
Descoberto <50559090+Descoberto@users.noreply.github.com>
2022-03-29 16:56:31 -0300

    Delete hello_cargo/target directory

--- hello_cargo/target/debug/deps/hello_cargo-e750a9b6cab672ac.d
+++
@@ -1,5 +0,0 @@
-C:\Users\e.ribeiro.rodrigues\projects\hello_cargo\target\debug\deps\hello_cargo-e750a9b6cab672ac.rmeta: src\main.rs
-
-C:\Users\e.ribeiro.rodrigues\projects\hello_cargo\target\debug\deps\hello_cargo-e750a9b6cab672ac.d: src\main.rs
-
-src\main.rs:
