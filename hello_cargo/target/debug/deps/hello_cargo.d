8d2a0440f73d757c002fa87fde1c4f0cd18a9053
Descoberto <50559090+Descoberto@users.noreply.github.com>
2022-03-29 16:55:55 -0300

    Hello Cargo with Rust.

---
+++ hello_cargo/target/debug/deps/hello_cargo.d
@@ -0,0 +1,5 @@
+C:\Users\e.ribeiro.rodrigues\projects\hello_cargo\target\debug\deps\hello_cargo.exe: src\main.rs
+
+C:\Users\e.ribeiro.rodrigues\projects\hello_cargo\target\debug\deps\hello_cargo.d: src\main.rs
+
+src\main.rs:
